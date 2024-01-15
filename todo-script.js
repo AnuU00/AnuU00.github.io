0003fa87cc3ba4dea8e201e808e3f4c3b2e6a2f3
Anushka Udayanga <117751600+AnuU00@users.noreply.github.com>
2024-01-15 11:57:49 +0530

    Initial commit

--- todo-script.js
+++ todo-script.js
@@ -1,82 +1,54 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBrrPYPa_KVcCoQkCcQ51CxUA7RDMQBO9I",
-    authDomain: "to-do-list-cef00.firebaseapp.com",
-    databaseURL: "https://to-do-list-cef00-default-rtdb.firebaseio.com",
-    projectId: "to-do-list-cef00",
-    storageBucket: "to-do-list-cef00.appspot.com",
-    messagingSenderId: "882467650863",
-    appId: "1:882467650863:web:331fea17840da9878df5da",
-    measurementId: "G-J9VG15WDD9"
-    };
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    const db = getDatabase(app);
+function addTask(){
+    let input = inputBox.value;
+    let trimedInput = input.trim();
 
-    const inputBox = document.getElementById("input-box");
-    const listContainer = document.getElementById("list-container");
+    if(trimedInput !== ''){
+        let li = document.createElement("li");
+        li.style.overflowWrap = "break-word";
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    
+    inputBox.value = "";
+    saveData()
+}
 
-    function addTask(){
-        let input = inputBox.value;
-        let trimedInput = input.trim();
-
-        if(trimedInput !== ''){
-            let li = document.createElement("li");
-            li.style.overflowWrap = "break-word";
-            li.innerHTML = inputBox.value;
-            listContainer.appendChild(li);
-            let span = document.createElement("span");
-            span.innerHTML = "\u00d7";
-            li.appendChild(span);
-        }
-        
-        inputBox.value = "";
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
         saveData()
     }
-
-    // Added event listener here
-    document.getElementById('add-button').addEventListener('click', addTask);
-    listContainer.addEventListener("click", function(e){
-        e.preventDefault();
-        if(e.target.tagName === "LI"){
-            e.target.classList.toggle("checked");
-            saveData()
-        }
-        else if(e.target.tagName === "SPAN"){
-            e.target.parentElement.remove();
-            saveData()
-        }
-    }, false);
-
-    function clearTasks(){
-        let checkedItems = document.querySelectorAll('.checked');
-
-        for(let i=0; i<checkedItems.length; i++){
-            checkedItems[i].remove();
-        }
-        saveData();
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
     }
+}, false);
 
-    window.addEventListener("scroll", function(){
-        let addDiv = document.querySelector("h2");
-        addDiv.classList.toggle("sticky", this.window.scrollY > 0);
-    })
+function clearTasks(){
+    let checkedItems = document.querySelectorAll('.checked');
 
-    function saveData(){
-        let tasks = listContainer.innerHTML;
-        set(ref(db, '/'), {
-            tasks: tasks
-        });
+    for(let i=0; i<checkedItems.length; i++){
+        checkedItems[i].remove();
     }
+    saveData();
+}
 
-    function showData(){
-        onValue(ref(db, '/'), (snapshot) => {
-            const data = snapshot.val();
-            if (data) {
-                listContainer.innerHTML = data.tasks;
-            }
-        });
-    }
-    showData();
+window.addEventListener("scroll", function(){
+    let addDiv = document.querySelector("h2");
+    addDiv.classList.toggle("sticky", this.window.scrollY > 0);
+})
+
+function saveData(){
+    localStorage.setItem("todo_data", listContainer.innerHTML);
+}
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("todo_data");
+}
+showData();
